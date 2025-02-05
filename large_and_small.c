227cd55ff3e3bf166a02050aea098edd56a51ddc
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2025-02-05 14:48:31 +0530

    Add files via upload

---
+++ large_and_small.c
@@ -0,0 +1,29 @@
+"""author: Daiji Kuriakose
+   Date  : 05/02/2025
+   Program: To input elements into an array and find the maximum and minimum elements.
+"""
+#include<stdio.h>
+int main(){
+ int limit,i,large,small,pos_large,pos_small,j;
+ printf("Enter the limit");
+ scanf("%d",&limit);
+ int number[limit];
+ for(i=0;i<=limit;i++){
+  scanf("%d",&number[i]);
+  }
+ large=number[0];
+ small=number[0];
+ for(j=0;j<=limit;j++){
+  if(number[j]>large){
+   large=number[j];
+   pos_large=j;
+  }
+  if(number[j]<small){
+   small=number[j];
+   pos_small=j;
+  }
+ }
+ printf("\nThe largest number is %d and position is %d ",large,pos_large);
+ printf("\nThe smallest number is %d and position is %d ",small,pos_small);
+ return 0;
+}
---
+++ search.c
@@ -0,0 +1,24 @@
+"""author: Daiji Kuriakose
+   Date  : 05/02/2025
+   Program: To search a number in a array.
+"""
+#include<stdio.h>
+int main(){
+int limit,i,num,j;
+ printf("Enter the limit: ");
+ scanf("%d",&limit);
+ int number[limit];
+ printf("Enter the elements: ");
+ for(i=0;i<limit;i++){
+  scanf("%d",&number[i]);
+ }
+ printf("Enter the number to search: ");
+ scanf("%d",&num);
+ for(j=0;j<=limit;j++){
+  if(num==number[j]){
+   printf("the number %d is present",num);
+   printf("\nThe position of the element is %d",j+1);
+  }
+ }
+return 0;
+}
