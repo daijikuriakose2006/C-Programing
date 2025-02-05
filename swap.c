f8e9c9505c57163b45193da792dc855402050e5d
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2025-02-05 16:27:56 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+ int limit,i,n,temp,j;
+ printf("Enter the limit");
+ scanf("%d",&limit);
+ int number[limit];
+ for(i=0;i<limit;i++){
+  scanf("%d",&number[i]);
+ }
+ for(i=0;i<limit-1;i++){
+  for(j=0;j<limit-i;j++){
+   if (number[j]>number[j+1]){
+    temp=number[j];
+    number[j]=number[j+1];
+    number[j+1]=temp;
+   }
+  }
+ }
+ printf("\n the sorted array: ");
+ for(i=0;i<limit;i++){
+  printf("%d\t",number[i]);
+ }
+ return 0;
+}
